e59dfacfa2181b5ac32a19fd9138ebd5be58aab9
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-12-10 13:00:33 +1000

    Добавьте файлы проекта.

---
+++ Pages/PageAdd.xaml.cs
@@ -0,0 +1,81 @@
+﻿using ISRPO_PR15_Cherednichenko.ApplicationData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO_PR15_Cherednichenko.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAdd.xaml
+    /// </summary>
+    public partial class PageAdd : Page
+    {
+        private Tovars _currentTovars = new Tovars();
+
+        public PageAdd(Tovars selectedTovars)
+        {
+            InitializeComponent();
+            if (selectedTovars != null)
+            {
+                _currentTovars = selectedTovars;
+            }
+
+            DataContext = _currentTovars;
+            ComboColour.ItemsSource = SofaEntities.GetContext().Colour.ToList();
+            ComboMather.ItemsSource = SofaEntities.GetContext().Matherial.ToList();
+
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.GoBack();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentTovars.nameTovar))
+                errors.AppendLine("Укажите название товара");
+            if (_currentTovars.type == null)
+                errors.AppendLine("Укажите тип товара (диван или кресло)");
+            if (_currentTovars.Colour == null)
+                errors.AppendLine("Укажите цвет товара");
+            if (_currentTovars.Matherial == null)
+                errors.AppendLine("Укажите материал товара");
+            if (_currentTovars.cena <= 0)
+                errors.AppendLine("Цена не может быть меньше или равна 0");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            //добавление
+            if (_currentTovars.idTovar == 0)
+                SofaEntities.GetContext().Tovars.Add(_currentTovars);
+            //сохранение
+            try
+            {
+                SofaEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                AppFrame.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ Pages/PageTovar.xaml.cs
@@ -0,0 +1,70 @@
+﻿using ISRPO_PR15_Cherednichenko.ApplicationData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISRPO_PR15_Cherednichenko.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageTovar.xaml
+    /// </summary>
+    public partial class PageTovar : Page
+    {
+        public PageTovar()
+        {
+            InitializeComponent();
+            DGTovars.ItemsSource = SofaEntities.GetContext().Tovars.ToList();
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new PageAdd(null));
+        }
+
+        private void btnRedact_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new PageAdd((sender as Button).DataContext as Tovars));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility.Visible == Visibility)
+            {
+                SofaEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGTovars.ItemsSource = SofaEntities.GetContext().Tovars.ToList();
+            }
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var _TovarForDel = DGTovars.SelectedItems.Cast<Tovars>().ToList();
+            if (MessageBox.Show("Вы действительно хотите удалить?", "Предупреждение", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    SofaEntities.GetContext().Tovars.RemoveRange(_TovarForDel);
+                    SofaEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    DGTovars.ItemsSource = SofaEntities.GetContext().Tovars.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
